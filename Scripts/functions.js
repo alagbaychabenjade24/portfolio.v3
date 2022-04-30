007baaa2b943cb2a4f88094dc3949b8f018451ef
Chaben Jade Alagbay <alagbaychabenjade24@gmail.com>
2022-04-30 21:42:10 +0800

    add scroll event remove on scroll for the menu and ul in the mobile format

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -1,22 +1,30 @@
 const menu = document.querySelector('header nav .menu');
 const navLinks = document.querySelector('header nav ul');
 const navLinksLists = document.querySelectorAll('header nav ul li');
+const main = document.querySelector('main');
 
 menu.addEventListener('click', () => {
-	menu.classList.toggle('close');
+	menu.classList.toggle('menu--clicked');
 
 	navLinks.classList.toggle('nav__links__active');
 
 	navLinksLists.forEach(navLinksList => {
 		navLinksList.addEventListener('click', () => {
-			menu.classList.toggle('close');
+			menu.classList.toggle('menu--clicked');
 
 			navLinks.classList.remove('nav__links__active');
 		});
 	});
 });
 
+main.addEventListener('click', () => {
+	menu.classList.remove('menu--clicked');
+
+	navLinks.classList.remove('nav__links__active');
+});
+
 const scrollTop = document.querySelector('.scroll__to__top');
+const header = document.querySelector('header');
 
 window.addEventListener('scroll', () => {
 	if (window.pageYOffset > 150) {
@@ -24,12 +32,14 @@ window.addEventListener('scroll', () => {
 	} else {
 		scrollTop.classList.remove('scroll__to__top--active');
 	}
-});
 
-const header = document.querySelector('header');
-
-window.addEventListener('scroll', function () {
 	const windowPosition = window.scrollY > 610;
 
 	header.classList.toggle('scrolling-active', windowPosition);
+
+	if (window.pageYOffset + 1 || window.pageYOffset - 1) {
+		menu.classList.remove('menu--clicked');
+
+		navLinks.classList.remove('nav__links__active');
+	}
 });
